64568e099f3fb70793834dbfdc41010ed2d8f90a
Sarthak Mukherjee <sarthakmukherjee@virtualemployee.com>
2026-04-16 14:32:45 +0530

    Changed main.py

--- backend/main.py
+++ backend/main.py
@@ -250,7 +250,8 @@ async def analyze_resume_with_text_jd(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
     
     
     # yeaaa
